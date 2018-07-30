4a7eb6ee8672ed6267577f595b4460941d5410a8
saikiran789 <32534662+saikiran789@users.noreply.github.com>
2018-07-30 10:33:05 +0530

    Update models.py

--- chat/models.py
+++ chat/models.py
@@ -50,24 +50,3 @@ class OnlineUsers(models.Model):
         """
         return "room-%s" % self.id
 	
-
-from django.contrib.auth.models import User
-from django.contrib.sessions.models import Session
-from django.utils import timezone
-
-def get_current_users():
-    active_sessions = Session.objects.filter(expire_date__gte=timezone.now())
-    user_id_list = []
-    for session in active_sessions:
-        data = session.get_decoded()
-        user_id_list.append(data.get('_auth_user_id', None))
-    # Query all logged in users based on id list
-    return User.objects.filter(id__in=user_id_list)
-
-queryset = get_current_users()
-print(queryset)
-print(queryset.exists())
-print(queryset.count())
-
-
-    
